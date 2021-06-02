7bca1c20f172d192651aaa05a16fadc34c3d08ea
章君涛 <zhangjuntao.eason@bytedance.com>
2021-06-02 15:48:17 +0800

    zjt

--- dice_loss.py
+++ dice_loss.py
@@ -29,11 +29,28 @@ class DiceCoeff(Function):
 
 
 def dice_coeff(input, target):
-    """Dice coeff for batches"""
-    if input.is_cuda:
-        s = torch.FloatTensor(1).cuda().zero_()
-    else:
-        s = torch.FloatTensor(1).zero_()
-    for i, c in enumerate(zip(input, target)):
-        s = s + DiceCoeff().forward(c[0], c[1])
-    return s / (i + 1)
+    # """Dice coeff for batches"""
+    # if input.is_cuda:
+    #     s = torch.FloatTensor(1).cuda().zero_()
+    # else:
+    #     s = torch.FloatTensor(1).zero_()
+    # for i, c in enumerate(zip(input, target)):
+    #     s = s + DiceCoeff().forward(c[0], c[1])
+    # return s / (i + 1)
+    """This definition generalize to real valued pred and target vector.
+    This should be differentiable.
+    pred: tensor with first dimension as batch
+    target: tensor with first dimension as batch
+    """
+
+    smooth = 1.
+
+    # have to use contiguous since they may from a torch.view op
+    iflat = input.contiguous().view(-1)
+    tflat = target.contiguous().view(-1)
+    intersection = (iflat * tflat).sum()
+
+    A_sum = torch.sum(iflat * iflat)
+    B_sum = torch.sum(tflat * tflat)
+
+    return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth))
--- train.py
+++ train.py
@@ -22,11 +22,28 @@ dir_img = './data/PhC-C2DL-PSC/01/'
 dir_mask = './data/PhC-C2DL-PSC/01_ST/SEG/'
 # dir_checkpoint = 'checkpoints/'
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Train the UNet on images and target masks',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=5,
+                        help='Number of epochs', dest='epochs')
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=1,
+                        help='Batch size', dest='batchsize')
+    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.00001,
+                        help='Learning rate', dest='lr')
+    parser.add_argument('-f', '--load', dest='load', type=str, default=False,
+                        help='Load model from a .pth file')
+    parser.add_argument('-s', '--scale', dest='scale', type=float, default=0.5,
+                        help='Downscaling factor of the images')
+    parser.add_argument('-v', '--validation', dest='val', type=float, default=10.0,
+                        help='Percent of the data that is used as validation (0-100)')
+
+    return parser.parse_args()
 
 def train_net(net,
               device,
               epochs=5,
-              batch_size=2,
+              batch_size=1,
               lr=0.00001,
               val_percent=0.1,
               save_cp=True,
@@ -129,23 +146,7 @@ def train_net(net,
     writer.close()
 
 
-def get_args():
-    parser = argparse.ArgumentParser(description='Train the UNet on images and target masks',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=5,
-                        help='Number of epochs', dest='epochs')
-    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
-                        help='Batch size', dest='batchsize')
-    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.00001,
-                        help='Learning rate', dest='lr')
-    parser.add_argument('-f', '--load', dest='load', type=str, default=False,
-                        help='Load model from a .pth file')
-    parser.add_argument('-s', '--scale', dest='scale', type=float, default=0.5,
-                        help='Downscaling factor of the images')
-    parser.add_argument('-v', '--validation', dest='val', type=float, default=10.0,
-                        help='Percent of the data that is used as validation (0-100)')
 
-    return parser.parse_args()
 
 
 if __name__ == '__main__':
